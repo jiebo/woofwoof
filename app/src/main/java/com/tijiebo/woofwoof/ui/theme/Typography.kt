1c0e671372ab2c7f74557f9bf9aa278cd7f5162a
Jie Bo <jie-bo.ti@expressvpn.com>
2025-01-13 01:31:26 +0800

    Added essential Compose theming

---
+++ app/src/main/java/com/tijiebo/woofwoof/ui/theme/Typography.kt
@@ -0,0 +1,22 @@
+package com.tijiebo.woofwoof.ui.theme
+
+import androidx.compose.material3.Typography
+
+private val typography = Typography()
+val AppTypography = Typography(
+    displayLarge = typography.displayLarge.copy(fontFamily = fontMontserrat),
+    displayMedium = typography.displayMedium.copy(fontFamily = fontMontserrat),
+    displaySmall = typography.displaySmall.copy(fontFamily = fontMontserrat),
+    headlineLarge = typography.headlineLarge.copy(fontFamily = fontMontserrat),
+    headlineMedium = typography.headlineMedium.copy(fontFamily = fontMontserrat),
+    headlineSmall = typography.headlineSmall.copy(fontFamily = fontMontserrat),
+    titleLarge = typography.titleLarge.copy(fontFamily = fontMontserrat),
+    titleMedium = typography.titleMedium.copy(fontFamily = fontMontserrat),
+    titleSmall = typography.titleSmall.copy(fontFamily = fontMontserrat),
+    bodyLarge = typography.bodyLarge.copy(fontFamily = fontMontserrat),
+    bodyMedium = typography.bodyMedium.copy(fontFamily = fontMontserrat),
+    bodySmall = typography.bodySmall.copy(fontFamily = fontMontserrat),
+    labelLarge = typography.labelLarge.copy(fontFamily = fontMontserrat),
+    labelMedium = typography.labelMedium.copy(fontFamily = fontMontserrat),
+    labelSmall = typography.labelSmall.copy(fontFamily = fontMontserrat)
+)
